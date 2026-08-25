84b284eb684b6db876b40eee09212094632faf93
sarvesh kumar <156599618+ChernobylManushya@users.noreply.github.com>
2026-08-25 08:08:20 +0530

    Update 24.cpp

--- 24.cpp
+++ 24.cpp
@@ -3,53 +3,40 @@
 #include <iostream>
 using namespace std;
 
-class Node
-{
+class Node{
 public:
     int data;
     Node* next;
 
-    Node(int value)
-    {
+    Node(int value){
         data = value;
         next = NULL;
     }
 };
 
-class LinkedList
-{
+class LinkedList{
 public:
     Node* head;
     Node* tail;
 
-    LinkedList()
-    {
+    LinkedList(){
         head = tail = NULL;
     }
-
-    void push(int value)
-    {
+    void push(int value){
         Node* newNode = new Node(value);
-
-        if (head == NULL)
-        {
+        if (head == NULL){
             head = tail = newNode;
         }
-        else
-        {
+        else{
             newNode->next = head;
             head = newNode;
         }
     }
 
-    void sort()
-    {
-        for (Node* i = head; i != NULL; i = i->next)
-        {
-            for (Node* j = i->next; j != NULL; j = j->next)
-            {
-                if (i->data > j->data)
-                {
+    void sort(){
+        for (Node* i = head; i != NULL; i = i->next){
+            for (Node* j = i->next; j != NULL; j = j->next){
+                if (i->data > j->data){
                     int temp = i->data;
                     i->data = j->data;
                     j->data = temp;
@@ -59,57 +46,43 @@ public:
     }
 };
 
-void bucketSort(int arr[], int n)
-{
+void bucketSort(int arr[], int n){
     LinkedList bucket[10];
-
     int max = arr[0];
-
-    for (int i = 1; i < n; i++)
-    {
+    for (int i = 1; i < n; i++){
         if (arr[i] > max)
             max = arr[i];
     }
 
     // Put elements into buckets
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++){
         int index = (arr[i] * 10) / (max + 1);
-
         bucket[index].push(arr[i]);
     }
 
     // Sort each bucket
-    for (int i = 0; i < 10; i++)
-    {
+    for (int i = 0; i < 10; i++){
         bucket[i].sort();
     }
 
     // Combine buckets
     int index = 0;
 
-    for (int i = 0; i < 10; i++)
-    {
+    for (int i = 0; i < 10; i++){
         Node* temp = bucket[i].head;
 
-        while (temp != NULL)
-        {
+        while (temp != NULL){
             arr[index++] = temp->data;
             temp = temp->next;
         }
     }
 }
 
-int main()
-{
+int main(){
     int arr[] = {42, 23, 4, 16, 8, 15, 9};
-
     int n = 7;
-
     bucketSort(arr, n);
-
     for (int i = 0; i < n; i++)
         cout << arr[i] << " ";
-
     return 0;
 }
